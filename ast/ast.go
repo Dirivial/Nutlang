7cef98c9e109ab4d77201b964469eeb79e2dbf9e
Dirivial <alexander.kadeby@gmail.com>
2024-01-18 10:35:38 +0100

    Parsing of array index operators

--- ast/ast.go
+++ ast/ast.go
@@ -43,6 +43,26 @@ func (p *Program) String() string {
 	return out.String()
 }
 
+type IndexExpression struct {
+	Token token.Token // The [ token
+	Left  Expression
+	Index Expression
+}
+
+func (ie *IndexExpression) expressionNode()      {}
+func (ie *IndexExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IndexExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString("[")
+	out.WriteString(ie.Index.String())
+	out.WriteString("])")
+
+	return out.String()
+}
+
 type ArrayLiteral struct {
 	Token    token.Token // the '[' token
 	Elements []Expression
