ffeb1ff3b3bbaf534bf6be1a80ee58c01db35a18
Dirivial <alexander.kadeby@gmail.com>
2024-01-19 06:11:52 +0100

    Added pop built-in

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -77,7 +77,7 @@ var builtins = map[string]*object.Builtin{
 			arr := args[0].(*object.Array)
 			length := len(arr.Elements)
 			if length > 0 {
-				newElements := make([]object.Object, length-1, length-1)
+				newElements := make([]object.Object, length-1)
 				copy(newElements, arr.Elements[1:length])
 				return &object.Array{Elements: newElements}
 			}
@@ -104,6 +104,33 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"pop": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `pop` must be ARRAY, got %s",
+					args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+
+			if length == 0 {
+				return &object.Array{Elements: []object.Object{}}
+			}
+			if length > 0 {
+
+				newElements := make([]object.Object, length-1, length-1)
+				copy(newElements, arr.Elements[0:length-1])
+
+				return &object.Array{Elements: newElements}
+			}
+			return NULL
 		},
 	},
 	"puts": {
